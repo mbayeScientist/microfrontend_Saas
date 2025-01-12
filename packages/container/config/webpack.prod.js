642e51e47eac321f32577a5dae18e2b66da33378
mbaye <mouhamed.mbaye@etu.univ-lyon1.fr>
2025-01-12 21:42:14 +0100

    deploiement github pages

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -2,14 +2,18 @@ const {merge} = require('webpack-merge');
 const commonConfig = require('./webpack.common.js');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const packageJson = require('../package.json');
-
+const path = require('path');
 
 const domain= process.env.PRODUCTION_DOMAIN;
 const prodConfig = {
     mode : 'production',
-output:{
-    filename : '[name].[contenthash].js'
-},
+    entry: './src/index.js', // Point d'entrée principal
+    output: {
+        path: path.resolve(__dirname, 'dist'), // Dossier de sortie
+        filename: '[name].[contenthash].js', // Nom des fichiers JS générés
+        publicPath: '/<repository-name>/', // Chemin public GitHub Pages
+        clean: true, // Nettoie le dossier dist avant chaque build
+    },
 plugins:[
     new ModuleFederationPlugin({
         name : 'container',
